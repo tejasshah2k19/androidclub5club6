f9919180da51cb2957973976c72c237b66ab6e6b
Tejas Shah <tejasshah2k19@gmail.com>
2025-07-13 16:36:43 +0530

    read data

--- app/src/main/java/com/royal/SignupActivity.java
+++ app/src/main/java/com/royal/SignupActivity.java
@@ -1,6 +1,10 @@
 package com.royal;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,6 +13,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SignupActivity extends AppCompatActivity {
+
+
+        EditText edtFirstName;
+        EditText edtLastName;
+    EditText edtEmail;
+    EditText edtPassword;
+    Button btnSubmit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +31,32 @@ public class SignupActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+    //bind - connect - link
+      edtFirstName =   findViewById(R.id.edtSignupFirstName);
+      edtLastName = findViewById(R.id.edtSignupLastName);
+      edtEmail = findViewById(R.id.edtSignupEmail);
+      edtPassword = findViewById(R.id.edtSignupPassword);
+      btnSubmit = findViewById(R.id.btnSignupSubmit);
+
+      //on button click
+      btnSubmit.setOnClickListener(new View.OnClickListener() {
+          @Override
+          public void onClick(View view) {
+              String firstName = edtFirstName.getText().toString();
+              String lastName = edtLastName.getText().toString();
+              String email = edtEmail.getText().toString();
+              String password  = edtPassword.getText().toString();
+
+              Log.i("SignupActivity",firstName);
+              Log.i("SignupActivity",lastName);
+              Log.i("SignupActivity",email);
+              Log.i("SignupActivity",password);
+
+          }
+      });
+
+
+
     }
 }
